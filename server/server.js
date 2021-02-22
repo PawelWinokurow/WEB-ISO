985aacfddff5a16ff8428e6e78bed7b862b11108
PawelWinokurow <pawelwinokurow@gmail.com>
2021-02-22 21:54:55 +0100

    Server encapsulated

--- server/server.js
+++ server/server.js
@@ -1,114 +1,144 @@
 var express = require('express');
 var schedule = require('node-schedule');
+var databaseService = require('./services/database_service');
+var soapService = require('./services/soap_service');
+var emailService = require('./services/email_service');
+var randomService = require('./services/random_service');
 var cors = require('cors');
 var logger = require('morgan');
 var fetch = require('node-fetch');
-var db_service = require('./services/database_service');
-var soap_service = require('./services/soap_service');
-var email_service = require('./services/email_service');
-var random_service = require('./services/random_service');
-var HttpsProxyAgent = require('https-proxy-agent');
+var httpsProxyAgent = require('https-proxy-agent');
+var validUrl = require('valid-url');
 
+require('dotenv').config();
 
-//db_service.connect();
-
-var app = express();
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(cors());
-
-// We need HttpsProxyAgent to use proxy for re-captcha
-var proxyAgent = new HttpsProxyAgent(process.env.EMAIL_PROXY);
+//databaseService.connect();
 
 /**
- * Runs each day at 00.00 and removes old not confirmed customer masks.
+ * Class to to manage the server. It contains node js express application
  */
-schedule.scheduleJob('0 0 * * *', function () {
-  db_service.removeOldMasks();
-});
-
-/**
- * Enpoint to get customer masks from application.
- */
-app.post("/request", function (req, res, next) {
-  let mask = req.body;
-  console.log(mask)
-  if (mask.isDirect) {
-    soap_service.sendMask(mask.sapMask);
-  } else {
-    const hash = random_service.generateHash();
-    db_service.storeMask(hash, mask.sapMask);
-    email_service.sendEmail(hash, mask.emailTo);
+class Server {
+  constructor() {
+    this.expressApp = express();
+    this.expressApp.use(logger('dev'));
+    this.expressApp.use(express.json());
+    this.expressApp.use(cors());
+    this.proxyAgent = null;
+    this.checkProxy();
+    this.runSchedule();
+    this.initEndPoints();
   }
-  res.json({
-    ok: true
-  });
-});
 
-/**
- * Endpoint to get email confirmations.
- */
-app.get("/confirm", function (req, res, next) {
-  db_service.checkConfirmation(req.query.hash).then(result => {
-      var mask = JSON.parse(result.mask)
-      soap_service.sendMask(mask);
-      res.send('<p>Success! The mask was confirmed.</p>');
-      next();
-    })
-    .catch(() => {
-      res.send('<p>Error! The mask was not confirmed.</p>');
-      next();
-    })
-});
+  checkProxy() {
+    if (validUrl.isUri(process.env.PROXY)) {
+      process.env.HTTP_PROXY = process.env.PROXY
+      process.env.HTTPS_PROXY = process.env.PROXY
 
-/**
- * Endpoint to get recaptcha token from the client.
- */
-app.post('/token_validate', (req, res) => {
-
-  let token = req.body.recaptcha;
-
-  if (token === null || token === undefined) {
-    res.status(201).send({
-      success: false,
-      message: "Token is empty or invalid"
-    })
-    return console.log("token empty");
+      // We need HttpsProxyAgent to use proxy for re-captcha
+      this.proxyAgent = new httpsProxyAgent(process.env.EMAIL_PROXY);
+      console.log('In Proxy');
+    }
   }
-  fetch(process.env.RECAPTCHA_HOST, {
-      agent: proxyAgent,
-      method: "post",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/x-www-form-urlencoded; charset=utf-8"
-      },
-      body: `secret=${process.env.RECAPTCHA_KEY}&response=${token}&remoteip=${req.socket.remoteAddress}`
-    })
-    .then(res => res.json()).catch(err => {
-      res.send({
-        success: false
-      });
-      return console.log(`Error: ${err}`);
-    })
-    .then(json => {
-      if (json.success !== undefined && !json.success) {
-        res.send({
-          success: false
-        });
+
+  /**
+   * Runs each day at 00.00 and removes old not confirmed customer masks
+   */
+  runSchedule() {
+    schedule.scheduleJob('0 0 * * *', function () {
+      databaseService.removeOldMasks();
+    });
+  }
+
+  initEndPoints() {
+    /**
+     * Enpoint to get customer masks from application.
+     */
+    this.expressApp.post("/request", function (req, res, next) {
+      let mask = req.body;
+      if (mask.isDirect) {
+        soapService.sendMask(mask.sapMask);
+      } else {
+        const hash = randomService.generateHash();
+        databaseService.storeMask(hash, mask.sapMask);
+        emailService.sendEmail(hash, mask.emailTo);
       }
-      console.log('captcha')
-      console.log(json)
-      //if passed response success message to client.
-      res.send({
-        success: true
+      res.json({
+        ok: true
       });
     });
-})
 
-app.listen(process.env.WEB_PORT, () => {
+    /**
+     * Endpoint to get email confirmations.
+     */
+    this.expressApp.get("/confirm", function (req, res, next) {
+      databaseService.checkConfirmation(req.query.hash).then(result => {
+          var mask = JSON.parse(result.mask)
+          soapService.sendMask(mask);
+          res.send('<p>Success! The mask was confirmed.</p>');
+          next();
+        })
+        .catch(() => {
+          res.send('<p>Error! The mask was not confirmed.</p>');
+          next();
+        })
+    });
 
-  //soap_service.test()
-  email_service.sendEmail('asdasdas', 'pawelwinokurow@gmail.com')
-  console.log(`WEB-ISO server is listening at http://localhost:${process.env.WEB_PORT}`)
-})
+    /**
+     * Endpoint to get recaptcha token from the client.
+     */
+    this.expressApp.post('/token_validate', (req, res) => {
+
+      let token = req.body.recaptcha;
+
+      if (token === null || token === undefined) {
+        res.status(201).send({
+          success: false,
+          message: "Token is empty or invalid"
+        })
+        return console.log("token empty");
+      }
+      var options = {
+        method: "post",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/x-www-form-urlencoded; charset=utf-8"
+        },
+        body: `secret=${process.env.RECAPTCHA_KEY}&response=${token}&remoteip=${req.socket.remoteAddress}`
+      }
+      if (this.proxyAgent) {
+        options.agent = proxyAgent;
+      }
+      fetch(process.env.RECAPTCHA_HOST, options)
+        .then(res => res.json()).catch(err => {
+          res.send({
+            success: false
+          });
+          return console.log(`Error: ${err}`);
+        })
+        .then(json => {
+          if (json.success !== undefined && !json.success) {
+            res.send({
+              success: false
+            });
+          }
+          console.log('captcha')
+          console.log(json)
+          //if passed response success message to client.
+          res.send({
+            success: true
+          });
+        })
+    });
+  }
+
+  start() {
+    this.expressApp.listen(process.env.WEB_PORT, () => {
+        //soap_service.test()
+        emailService.sendEmail('asdasdas', 'pawelwinokurow@gmail.com')
+      console.log(`WEB-ISO server is listening at http://localhost:${process.env.WEB_PORT}`)
+    })
+  }
+
+}
+
+new Server().start()
--- server/services/email_service.js
+++ server/services/email_service.js
@@ -1,45 +1,37 @@
 var nodemailer = require('nodemailer');
 require('dotenv').config()
 
-/*
-var transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST, 
-    port: process.env.EMAIL_PORT,
-    secure: false,
-    proxy: process.env.EMAIL_PROXY,
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASSWORD
-    }
-});
-*/
-
-
-var transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, // use SSL
-    proxy: process.env.EMAIL_PROXY,
-    auth: {
-        user: 'paulweinmacher@googlemail.com',
-        pass: 'Tromboman8919'
-    }
-});
-
 /**
- * Sends email.
+ * Sends emails.
  * @param  {string} hash Hash allows to identificate the requestor
  * @param  {string} emailTo Email of the requestor
  */
 exports.sendEmail = function (hash, emailTo) {
-    var mailOptions = {
+
+    emailOptions = {
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true, // use SSL
+        auth: {
+            user: 'paulweinmacher@googlemail.com',
+            pass: 'Tromboman8919'
+        }
+    }
+    
+    if (process.env.HTTP_PROXY != undefined) {
+        emailOptions.proxy = process.env.EMAIL_PROXY;
+    }
+    var transporter = nodemailer.createTransport(emailOptions);
+
+
+    var message = {
         from: "BayWa",
         to: emailTo,
         subject: 'Mask confirmation',
         html: '<p>Click <a href="http://localhost:3000/confirm?hash=' + hash + '">here</a> to confirm the mask.</p>'
     };
 
-    transporter.sendMail(mailOptions, function (error, info) {
+    transporter.sendMail(message, function (error, info) {
         if (error) {
             console.log(error);
         } else {
