af7c46f89e88680212090fb1ad603387041f5101
Pawel_Winokurow <pawelwinokurow@gmail.com>
2021-03-16 19:59:04 +0100

    Updated language dropdown design

--- client/src/app/components/main/main.component.ts
+++ client/src/app/components/main/main.component.ts
@@ -15,6 +15,8 @@ import { TokenProlongationService } from 'src/app/services/token-prolongation.se
 })
 export class MainComponent implements OnInit {
 
+  selectedLanguage = 'DE';
+
   constructor(private router: Router, public dictionaryService: DictionaryService, 
     public storageService: StorageService, public authService: AuthService, 
     private tokenProlongationService: TokenProlongationService){
@@ -42,6 +44,7 @@ export class MainComponent implements OnInit {
    * Click on the switch language button triggers this method. The method changes the current language from EN to DE and from DE to EN.
    */
   changeLanguage(){
+    this.selectedLanguage = this.selectedLanguage === 'EN' ? 'DE' : 'EN';
     this.dictionaryService.switchLanguage();
   }
 
