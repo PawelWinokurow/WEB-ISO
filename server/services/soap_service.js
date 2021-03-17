cc2cc582009b83401d26a2547a10b9aa6bf86a1d
Pawel_Winokurow <pawelwinokurow@gmail.com>
2021-03-17 09:32:29 +0100

    WIP

--- server/services/soap_service.js
+++ server/services/soap_service.js
@@ -5,13 +5,14 @@ const soap = require('soap');
  * @param {object} customer  
  * @param {string} wsdlUrl path to wsdl
  */
-function sendCustomer(envelope, wsdlUrl) {
+function sendCustomer(customer, wsdlUrl) {
+    console.log(customer)
     soap.createClient(wsdlUrl, function (err, soapClient) {
         if (err) {
             console.log("error", err);
         }
         soapClient.setSecurity(new soap.BasicAuthSecurity(process.env.SOAP_USER, process.env.SOAP_PASSWORD))
-        soapClient.SI_ISO_MGB_BAPI_MAINTAIN_PARTNER_outbound(envelope, function (err, result, raw, headers) {
+        soapClient.SI_ISO_MGB_BAPI_MAINTAIN_PARTNER_outbound(customer, function (err, result, raw, headers) {
             if (err) {
                 //console.log(err);
             }
