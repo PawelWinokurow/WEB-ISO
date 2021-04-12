e8a0a327185090263a404fd42a666ad22568c58f
PawelWinokurow <pawelwinokurow@gmail.com>
2021-04-12 15:23:44 +0200

    Tested after refactoring

--- client/src/app/services/account.service.spec.ts
+++ client/src/app/services/account.service.spec.ts
@@ -4,14 +4,13 @@ import { Account, AccountDTO } from 'src/app/interfaces/account';
 import { AccountService } from './account.service';
 import { AuthService } from './auth.service';
 
-
 describe('AccountService', () => {
   let accountService: AccountService;
   let authService: AuthService;
 
   const newAccount: Account = {
     username: 'user4',
-    email: 'pawelwinokurow@gmail.com',
+    email: 'user4@user4.com',
     companyCode: '1100',
     role: 'USER',
     password: 'password',
@@ -24,17 +23,17 @@ describe('AccountService', () => {
         ...services
       ],
       imports: [
-        ...externalModules
+        ...externalModules,
       ]
-    })
-      .compileComponents();
+    }).compileComponents();
     accountService = TestBed.inject(AccountService);
     authService = TestBed.inject(AuthService);
     await authService.login('admin', 'admin');
+    await accountService.deleteAccount<AccountDTO>(newAccount).toPromise();
+    await accountService.createAccount<AccountDTO>(newAccount).toPromise();
   });
 
   it('createAccount', async () => {
-    await accountService.createAccount<AccountDTO>(newAccount).toPromise();
     const accounts = await accountService.getAccounts<[Account]>().toPromise();
     const expectation = accounts.some(acc =>
       acc.username === newAccount.username
@@ -42,46 +41,37 @@ describe('AccountService', () => {
       && acc.companyCode === newAccount.companyCode
       && acc.role === newAccount.role
     );
-    await accountService.deleteAccount<AccountDTO>(newAccount).toPromise();
     expect(expectation).toBeTrue();
   });
-
-  it('deleteAccount', async () => {
-    await accountService.createAccount<AccountDTO>(newAccount).toPromise();
-    await accountService.deleteAccount<AccountDTO>(newAccount).toPromise();
-    const accounts = await accountService.getAccounts<[Account]>().toPromise();
-    const expectation = accounts.some(acc =>
-      acc.username === newAccount.username
-      && acc.email === newAccount.email
-      && acc.companyCode === newAccount.companyCode
-      && acc.role === newAccount.role
-      && acc.blocked == newAccount.blocked
-    );
-    expect(expectation).toBeFalse();
-  });
-
-  it('updateAccount', async () => {
-    //await accountService.createAccount<AccountDTO>(newAccount).toPromise();
-    let admin: Account = {
-      username: 'admin',
-      email: 'admin@admin.de',
-      companyCode: '1200',
-      role: 'ADMIN',
-      password: 'admin',
-      passwordOld: 'admin',
-      blocked: false
-    }
-    let updatedAccount = await accountService.updateAccount<AccountDTO>(admin).toPromise();
-
-    expect(updatedAccount && 'account' in updatedAccount).toBeTrue();
-  });
-
-  it('blockAccount', async () => {
-    await accountService.createAccount<AccountDTO>(newAccount).toPromise();
-    newAccount.blocked = !newAccount.blocked;
-    let blockedAccount = await accountService.blockAccount<AccountDTO>(newAccount).toPromise();
-    await accountService.deleteAccount<AccountDTO>(newAccount).toPromise();
-    expect(blockedAccount && blockedAccount.account.blocked == newAccount.blocked).toBeTrue();
-  });
-
+  
+    it('deleteAccount', async () => {
+      await accountService.deleteAccount<AccountDTO>(newAccount).toPromise();
+      const accounts = await accountService.getAccounts<[Account]>().toPromise();
+      const expectation = accounts.some(acc =>
+        acc.username === newAccount.username
+        && acc.email === newAccount.email
+        && acc.companyCode === newAccount.companyCode
+        && acc.role === newAccount.role
+        && acc.blocked == newAccount.blocked
+      );
+      expect(expectation).toBeFalse();
+    });
+  
+    it('updateAccount', async () => {
+      let changedAccount: Account = {
+        ...newAccount
+      }
+      delete changedAccount.password
+      delete changedAccount.passwordOld
+      changedAccount.companyCode = '9999'
+      let updatedAccount = await accountService.updateAccount<AccountDTO>(changedAccount).toPromise();
+      expect(updatedAccount && 'account' in updatedAccount).toBeTrue();
+    });
+  
+    it('blockAccount', async () => {
+      newAccount.blocked = !newAccount.blocked;
+      let blockedAccount = await accountService.blockAccount<AccountDTO>(newAccount).toPromise();
+      expect(blockedAccount && blockedAccount.account.blocked == newAccount.blocked).toBeTrue();
+    });
+  
 });
