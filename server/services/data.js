e61a0b61bea8b42ab92442f85a998697da73124a
PawelWinokurow <pawelwinokurow@gmail.com>
2021-04-19 14:39:19 +0200

    WIP

--- server/services/data.js
+++ server/services/data.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 async function storeFiles(files, hash) {
     if (files && files.length) {
-        const upload_dir = path.join(process.env.UPLOAD_FOLDER, hash);
+        const upload_dir = path.join(process.env.UPLOAD_DIRECTORY, hash);
         await fs.promises.mkdir(upload_dir, { recursive: true })
         for (let file of files) {
             const base64data = file.content.replace(/^data:.*,/, '');
