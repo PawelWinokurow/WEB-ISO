5067328f08d14765f00ebd0c7a2f971f176fd4bb
Pawel_Winokurow <pawelwinokurow@gmail.com>
2021-04-30 09:11:49 +0200

    WIP

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -5,6 +5,11 @@ const errorHandler = require('../middlewares/error');
 const databaseService = require('../services/database');
 const cryptoService = require('../services/crypto');
 const PRIVATE_KEY = fs.readFileSync(path.join(process.cwd(), process.env.PRIVATE_KEY));
+const ldap = require('ldapjs');
+const clientLDAP = ldap.createClient({
+    url: req.body.serverUrl
+});
+
 
 async function refreshToken(req, res) {
     try {
@@ -22,6 +27,58 @@ async function refreshToken(req, res) {
     }
 }
 
+//$dn = "CN={$username},OU=D,OU=standarduser,OU=X-RIS,DC=root,DC=local";
+async function login(req, res) {
+    try {
+        const requestAccount = req.body.account;
+
+        // LDAP Connection Settings
+        const server = "dns or ip here"; // 192.168.1.1
+        const userPrincipalName = ""; // Username
+        const password = "myPassword"; // User password
+        const adSuffix = "dc=root,dc=local"; // test.com
+
+        // Create client and bind to AD
+        const client = ldap.createClient({
+            url: `ldap://${server}`
+        });
+
+        client.bind(userPrincipalName, password, err => {
+            assert.ifError(err);
+        });
+
+        // Search AD for user
+        const searchOptions = {
+            scope: "sub",
+            filter: `(userPrincipalName=${userPrincipalName})`
+        };
+
+        client.search(adSuffix, searchOptions, (err, res) => {
+            assert.ifError(err);
+
+            res.on('searchEntry', entry => {
+                console.log(entry.object.name);
+            });
+            res.on('searchReference', referral => {
+                console.log('referral: ' + referral.uris.join());
+            });
+            res.on('error', err => {
+                console.error('error: ' + err.message);
+            });
+            res.on('end', result => {
+                console.log(result);
+            });
+        });
+
+        // Wrap up
+        client.unbind(err => {
+            assert.ifError(err);
+        });
+    } catch (e) {
+        errorHandler.unknownErrorResponse(e, 401);
+    }
+}
+/*
 async function login(req, res) {
     try {
         const requestAccount = req.body.account;
@@ -41,6 +98,7 @@ async function login(req, res) {
         errorHandler.unknownErrorResponse(e, 401);
     }
 }
+*/
 
 function createJWT(account) {
     const jwtBearerToken = jwt.sign(account, PRIVATE_KEY, {
